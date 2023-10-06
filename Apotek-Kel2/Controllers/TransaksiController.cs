821bbdc7f402d0fa00191595ae8e9351d8c71891
IkaAuliaPutri <ikaauliaputri5@gmail.com>
2023-10-06 10:50:10 +0700

    Error Obat

---
+++ Apotek-Kel2/Controllers/TransaksiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apotek_Kel2.Controllers
+{
+    public class TransaksiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Apotek-Kel2/Models/Obat.cs
+++ Apotek-Kel2/Models/Obat.cs
@@ -4,18 +4,16 @@ namespace Apotek_Kel2.Models
 {
     public class Obat
     {
-        // Tambahkan properti jenisObatList
         public String id { get; set; }
 
         [Required(ErrorMessage = "Nama Obat wajib diisi.")]
         public string namaObat { get; set; }
 
-        [Required(ErrorMessage = "Merk Obat wajib diisi.")]
-     
-        public String id_jenis { get; set; }
-
         [Required(ErrorMessage = "Jenis Obat wajib diisi.")]
-        
+        public string id_jenis { get; set; }
+
+        [Required(ErrorMessage = "Harga Obat wajib diisi.")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Harga Obat hanya boleh mengandung angka.")]
         public float hargaObat { get; set; }
 
         [Required(ErrorMessage = "Stok Obat wajib diisi.")]
@@ -24,7 +22,7 @@ namespace Apotek_Kel2.Models
 
         [Required(ErrorMessage = "Tanggal Kadaluarsa wajib diisi.")]
         [RegularExpression(@"^\d{4}-\d{2}-\d{2}$", ErrorMessage = "Format tanggal tidak valid. Gunakan format yyyy-MM-dd.")]
-        public String expObat { get; set; }
+        public string expObat { get; set; }
 
     }
 }
